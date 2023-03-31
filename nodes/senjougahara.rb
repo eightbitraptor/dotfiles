024a110552645a0c011ee9f6e899d16a6ec8c9fd
Matt Valentine-House <matt@eightbitraptor.com>
2023-03-31 20:52:54 +0100

    Switch swaybar -> waybar

--- nodes/senjougahara.rb
+++ nodes/senjougahara.rb
@@ -1,4 +1,8 @@
 node.reverse_merge!(
+  waybar_modules_left: ["sway/workspaces", "sway/mode"],
+  waybar_modules_center: ["mpd"],
+  waybar_modules_right: ["tray", "memory", "cpu", "pulseaudio", "network", "custom/weather", "clock"],
+
   mconfig: <<~MCONFIG
     output 'Dell Inc. DELL U2515H 9X2VY5630BML' pos 0 0 transform 270
     output 'Goldstar Company Ltd LG HDR 4K 0x00007E8D' pos 1440 500 scale 1.25
--- recipes/sway.rb
+++ recipes/sway.rb
@@ -8,6 +8,7 @@ packages = %w{
   swaylock
   wofi
   light
+  waybar
 }.each do |pkg_name|
   package pkg_name do
     action :install
@@ -17,12 +18,23 @@ end
 dotfiles = {
   ".config/sway/swayexit" => "sway/swayexit",
   ".config/sway/status.sh" => "sway/status.#{node.hostname}.sh",
+  ".config/waybar/style.css" => "waybar/style.css",
+  ".config/waybar/modules/battery.py" => "waybar/battery.py"
 }
 
 dotfile_template ".config/sway/config" do
   source "sway/config.erb"
   variables(
     monitor_config: node.mconfig
+  )
+end
+
+dotfile_template ".config/waybar/config" do
+  source "waybar/config.erb"
+  variables(
+    modules_left: node.waybar_modules_left,
+    modules_center: node.waybar_modules_center,
+    modules_right: node.waybar_modules_right
   )
 end
 
