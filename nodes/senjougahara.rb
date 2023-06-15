c40352c4d5470e0c9b47539d54af99b60b89a2fa
Matt Valentine-House <matt@eightbitraptor.com>
2023-06-15 12:11:30 +0100

    I've got both my monitors horizontally now

--- nodes/senjougahara.rb
+++ nodes/senjougahara.rb
@@ -4,8 +4,8 @@ node.reverse_merge!(
   waybar_modules_right: ["tray", "memory", "cpu", "pulseaudio", "network", "custom/weather", "clock"],
 
   mconfig: <<~MCONFIG
-    output 'Dell Inc. DELL U2515H 9X2VY5630BML' pos 0 0 transform 270
-    output 'Goldstar Company Ltd LG HDR 4K 0x00007E8D' pos 1440 500 scale 1.25
+    output 'Dell Inc. DELL U2515H 9X2VY5630BML' pos 0 0
+    output 'Goldstar Company Ltd LG HDR 4K 0x00007E8D' pos 2560 0 scale 1.25
   MCONFIG
 )
 
