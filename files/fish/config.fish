6af3d424db7bb4a50f3c5026c18097caf9a82461
Matt Valentine-House <matt@eightbitraptor.com>
2024-01-29 09:05:27 +0000

    Make the Fish template accurately reflect the disk

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -2,19 +2,19 @@ if set -q ALACRITTY_LOG
   cat ~/.cache/wal/sequences
 end
 
-set -Ux QT_IM_MODULE fcitx
+# set -Ux QT_IM_MODULE fcitx
 set -Ux XMODIFIERS @im=fcitx
-set -Ux GTK_IM_MODULE fcitx
+# set -Ux GTK_IM_MODULE fcitx
 
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux GTK_IM_MODULE fcitx
-set -Ux QT_IM_MODULE fxitx
-set -Ux XMODIFIERS @im=fcitx
+direnv hook fish | source
 
 source /usr/local/share/chruby/chruby.fish
 
-direnv hook fish | source
+chruby 3.3.0
+
+#status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
