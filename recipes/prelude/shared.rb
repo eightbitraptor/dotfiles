35bbcd7a203e35a24c4f8a3302dee7f1ab1aa4ad
Matt Valentine-House <matt@eightbitraptor.com>
2024-03-25 15:16:12 +0000

    Install my scripts repo

--- recipes/prelude/shared.rb
+++ recipes/prelude/shared.rb
@@ -4,3 +4,11 @@ dotfiles = {
 }
 
 dotfile dotfiles
+
+unless node.hostname == "spin"
+  git "Personal scripts" do
+    repository "git@github.com:eightbitraptor/scripts"
+    user node.user
+    destination "#{node.home_dir}/.scripts"
+  end
+end
