abd93f4e676433673cce0524b09f48e5be2545f2
Matt Valentine-House <matt@eightbitraptor.com>
2025-05-16 08:02:12 +0000

    Install ruby-install alongside chruby

--- recipes/chruby.rb
+++ recipes/chruby.rb
@@ -9,27 +9,31 @@ packages.each do |package_pairs|
   end
 end
 
-chruby_version = "0.3.9"
+versions = [
+  ["chruby", "0.3.9", "/usr/local/share/chruby/chruby.sh"],
+  ["ruby-install", "0.10.1", "/usr/local/bin/ruby-install"],
+]
 
-execute "Downloading Chruby" do
-  command "wget -O /tmp/chruby.tar.gz https://github.com/postmodern/chruby/archive/v#{chruby_version}.tar.gz" 
-  cwd "/tmp"
-  not_if "test -e /usr/local/share/chruby/chruby.sh"
+versions.each do |(name, version, path)|
+  execute "Downloading #{name}" do
+    command "wget -O /tmp/#{name}.tar.gz https://github.com/postmodern/#{name}/archive/v#{version}.tar.gz"
+    cwd "/tmp"
+    not_if "test -e #{path}"
+  end
+
+  execute "Extract #{name}" do
+    command "tar zxvf /tmp/#{name}.tar.gz"
+    cwd "/tmp"
+    only_if "test -e /tmp/#{name}.tar.gz"
+  end
+
+  execute "install #{name}" do
+    command "cd /tmp/#{name}-#{version} && make install"
+    only_if "test -d /tmp/#{name}-#{version}"
+  end
+
+  execute "Cleanup #{name}" do
+    command "rm -rf /tmp/#{name}-#{version} /tmp/#{name}.tar.gz"
+    only_if "test -e /tmp/#{name}.tar.gz || test -e /tmp/#{name}-#{version}"
+  end
 end
-
-execute "Extract Chruby" do
-  command "tar zxvf /tmp/chruby.tar.gz"
-  cwd "/tmp"
-  only_if "test -e /tmp/chruby.tar.gz"
-end
-
-execute "install Chruby" do
-  command "cd /tmp/chruby-#{chruby_version} && make install"
-  only_if "test -d /tmp/chruby-#{chruby_version}"
-end
-
-execute "Cleanup Chruby" do
-  command "rm -rf /tmp/chruby-#{chruby_version} /tmp/chruby.tar.gz"
-  only_if "test -e /tmp/chruby.tar.gz || test -e /tmp/chruby-#{chruby_version}"
-end
-
